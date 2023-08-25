a86b8846de438d3c8a6bf77085a12a47f02f3264
PavelZotov84 <pavlozotov1984@yandex.ru>
2023-08-25 18:41:31 +0300

    v2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+   public static Connection getConnection(){
+       Connection conn= null;
+       try {
+           conn= DriverManager.
+                   getConnection("jdbc:mysql://localhost:3306/mydbase","Pavel","root");
+           System.out.println("Connection ok");
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
+       return conn;
+   }
+
 }
